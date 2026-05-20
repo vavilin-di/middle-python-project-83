d446359694b3ce868fdb74041df58cd6115478b3
div <div@nowhe.re>
2026-05-20 17:17:32 +0700

    Исправлена опечатка.

--- database.sql
+++ database.sql
@@ -12,9 +12,9 @@ CREATE TABLE url_checks (
     id SERIAL NOT NULL, 
     url_id INTEGER NOT NULL, 
     status_code INTEGER, 
-    h1 TEXT(), 
-    title TEXT(), 
-    description TEXT(), 
+    h1 TEXT, 
+    title TEXT, 
+    description TEXT, 
     created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL, 
     PRIMARY KEY (id), 
     FOREIGN KEY(url_id) REFERENCES urls (id)
